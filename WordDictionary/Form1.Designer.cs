3d9f90cbc1712b2cb99d07c216ba1d69c011e5b8
VyacheslavTretyak <tretyak1c@gmail.com>
2018-11-07 09:44:19 +0200

    finished project

--- WordDictionary/Form1.Designer.cs
+++ WordDictionary/Form1.Designer.cs
@@ -31,18 +31,20 @@
 			this.textBoxURI = new System.Windows.Forms.TextBox();
 			this.buttonGet = new System.Windows.Forms.Button();
 			this.listBox = new System.Windows.Forms.ListBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.progressBar = new System.Windows.Forms.ProgressBar();
 			this.SuspendLayout();
 			// 
 			// textBoxURI
 			// 
-			this.textBoxURI.Location = new System.Drawing.Point(13, 13);
+			this.textBoxURI.Location = new System.Drawing.Point(12, 29);
 			this.textBoxURI.Name = "textBoxURI";
 			this.textBoxURI.Size = new System.Drawing.Size(416, 20);
 			this.textBoxURI.TabIndex = 0;
 			// 
 			// buttonGet
 			// 
-			this.buttonGet.Location = new System.Drawing.Point(448, 9);
+			this.buttonGet.Location = new System.Drawing.Point(13, 55);
 			this.buttonGet.Name = "buttonGet";
 			this.buttonGet.Size = new System.Drawing.Size(75, 23);
 			this.buttonGet.TabIndex = 1;
@@ -52,16 +54,34 @@
 			// listBox
 			// 
 			this.listBox.FormattingEnabled = true;
-			this.listBox.Location = new System.Drawing.Point(13, 40);
+			this.listBox.Location = new System.Drawing.Point(13, 92);
 			this.listBox.Name = "listBox";
-			this.listBox.Size = new System.Drawing.Size(416, 394);
+			this.listBox.Size = new System.Drawing.Size(416, 342);
 			this.listBox.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(93, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "input web address";
+			// 
+			// progressBar
+			// 
+			this.progressBar.Location = new System.Drawing.Point(111, 54);
+			this.progressBar.Name = "progressBar";
+			this.progressBar.Size = new System.Drawing.Size(317, 23);
+			this.progressBar.TabIndex = 4;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(541, 450);
+			this.ClientSize = new System.Drawing.Size(444, 450);
+			this.Controls.Add(this.progressBar);
+			this.Controls.Add(this.label1);
 			this.Controls.Add(this.listBox);
 			this.Controls.Add(this.buttonGet);
 			this.Controls.Add(this.textBoxURI);
@@ -77,6 +97,8 @@
 		private System.Windows.Forms.TextBox textBoxURI;
 		private System.Windows.Forms.Button buttonGet;
 		private System.Windows.Forms.ListBox listBox;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ProgressBar progressBar;
 	}
 }
 
--- WordDictionary/Form1.cs
+++ WordDictionary/Form1.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,66 @@ namespace WordDictionary
 {
 	public partial class Form1 : Form
 	{
+		private Dictionary<string, int> dic = new Dictionary<string, int>();
 		public Form1()
 		{
 			InitializeComponent();
+			buttonGet.Click += ButtonGet_Click;			
 		}
+
+		private void ButtonGet_Click(object sender, EventArgs e)
+		{
+			listBox.Items.Clear();
+			buttonGet.Enabled = false;
+			Task.Run(() => GetWords());
+		}
+
+		private void GetWords()
+		{
+			WebClient client = new WebClient();			
+			byte[] urlData = client.DownloadData(textBoxURI.Text); 			
+			string page = Encoding.UTF8.GetString(urlData);
+			string pattern = @">(.[^(<|>)]*)<";			
+			RegexOptions options = RegexOptions.Multiline;
+			int matches = Regex.Matches(page, pattern, options).Count;
+			int n = 0;
+			foreach (Match m in Regex.Matches(page, pattern, options))
+			{
+				string str = m.Groups[1].ToString();
+				string[] split = str.Split(" .,[]{}()-+=_;:\"'\t".ToCharArray());
+				foreach (string s in split)
+				{
+					string t = s.Trim();
+					int count = 0;
+					if (dic.Keys.Contains(t))
+					{
+						count = dic[t];
+					}
+					dic[t] = ++count;
+				}
+				progressBar.Invoke(new Action(()=>
+				{
+					float v = ((float)n / matches) * 100f;
+					progressBar.Value = (int)v;
+				}));
+				n++;
+			}
+			foreach(var val in dic)
+			{
+				listBox.Invoke(new Action(() =>
+				{
+					listBox.Items.Add($"{val.Key} : {val.Value}");
+				}));
+				
+			}
+			buttonGet.Invoke(new Action(()=>{
+				buttonGet.Enabled = true;
+				progressBar.Value = 0;
+			}));
+		}
+
+
+
+
 	}
 }
